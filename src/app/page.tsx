0b7f966f2a59010ab95f86f3b300bb3fbc37bf29
Caleb Carter <calebcarter@Ronalds-MBP.fios-router.home>
2024-10-03 18:10:05 -0400

    Fourth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
             <p className="text-lg text-gray-700 mb-4">
               Here's some placeholder text! Massa urna magnis dignissim id euismod porttitor vitae etiam viverra nunc at adipiscing sit morbi aliquet mauris porttitor nisi, senectus pharetra, ac porttitor orci.
             </p>
-            <a href="/blog/posts" className={`outline outline-1 outline-offset-2 border-purple-700 text-purple-700 hover:text-white py-2 px-4 rounded hover:bg-purple-800 md:w-auto ${styles.fit_content}`}>
+            <a href="/blog/posts" className={`outline outline-1 outline-offset-2 border-emerald-700 text-emerald-400 hover:text-white py-2 px-4 rounded hover:bg-emerald-800 md:w-auto ${styles.fit_content}`}>
               Go to My Blog
             </a>
           </div>
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_wisteria}`}></div>
+      <div className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_green}`}></div>
     </main>
   );
 }
